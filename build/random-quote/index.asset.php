34f6bf68a76ea9a861661a5fc4761ddb9bdc1f0b
Huzaifa Al Mesbah <hi@huzaifa.im>
2025-03-13 14:47:53 +0600

    Workflows update

--- build/random-quote/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '83bb8e464609a2ae974c');
--- build/random-quote/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '889aedb7fa56d53261c7');
